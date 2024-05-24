4df1a5814b5ab7a3fb40a8e9fffc644ebf46375c
rifkyariy <rifkyariy@gmail.com>
2024-05-25 06:05:42 +0700

    fix: fix link and add media

--- pages/medias.vue
+++ pages/medias.vue
@@ -14,22 +14,27 @@
       </div>
     </div>
 
+    <!-- Media List -->
     <div
       class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-12 xl:grid-cols-12 gap-2"
     >
-      <div v-for="(media, index) in mediaList" :key="index" class="relative">
-        <button @click="openModal(index)">
+      <div
+        v-for="(media, index) in mediaList"
+        :key="index"
+        class="relative w-full"
+      >
+        <button class="w-full" @click="openModal(index)">
           <img
             v-if="media.type === 'image'"
             :src="media.url"
             alt="Image"
-            class="w-full h-auto rounded-lg shadow-lg cursor-pointer"
+            class="w-full h-auto rounded-lg shadow-lg cursor-pointer img-thumbnail"
           />
-          <video
+          <img
             v-else-if="media.type === 'video'"
-            :src="media.url"
-            class="w-full h-auto rounded-lg shadow-lg cursor-pointer"
-          ></video>
+            src="https://cdn.discordapp.com/attachments/956377183121711194/1243701131088232578/video.png?ex=66526e70&is=66511cf0&hm=0500e775ced570853588037c9d91348bb1c81d7235d96578d129fb952d6ca7d9&"
+            class="w-full h-auto rounded-lg shadow-lg cursor-pointer img-thumbnail"
+          />
           <span
             v-if="media.isNew"
             class="absolute top-2 left-2 bg-red-600 text-white text-xs font-semibold py-1 px-2 rounded"
@@ -44,78 +49,91 @@
     <div
       v-if="modalMedia !== null"
       class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
+      @click="closeModalOutside"
     >
-      <div class="relative bg-gray-900 rounded-lg p-4 max-w-3xl w-full">
-        <button
-          class="absolute top-2 right-2 text-gray-400"
-          @click="closeModal"
+      <div
+        class="relative bg-gray-900 rounded-lg p-4 max-w-3xl w-full"
+        @click.stop
+      >
+        <!-- Modal -->
+        <div
+          v-if="modalMedia !== null"
+          class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
         >
-          <svg
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
-        </button>
+          <div class="relative bg-gray-900 rounded-lg p-4 max-w-3xl w-full">
+            <button
+              class="absolute top-2 right-2 text-gray-400"
+              @click="closeModal"
+            >
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
 
-        <button
-          class="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-400 bg-gray-800 rounded-full p-2"
-          @click="prevMedia"
-        >
-          <svg
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 19l-7-7 7-7"
-            ></path>
-          </svg>
-        </button>
+            <button
+              class="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-400 bg-gray-800 rounded-full p-2"
+              @click="prevMedia"
+            >
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 19l-7-7 7-7"
+                ></path>
+              </svg>
+            </button>
 
-        <button
-          class="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-400 bg-gray-800 rounded-full p-2"
-          @click="nextMedia"
-        >
-          <svg
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M9 5l7 7-7 7"
-            ></path>
-          </svg>
-        </button>
+            <button
+              class="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-400 bg-gray-800 rounded-full p-2"
+              @click="nextMedia"
+            >
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5l7 7-7 7"
+                ></path>
+              </svg>
+            </button>
 
-        <div>
-          <img
-            v-if="modalMedia.type === 'image'"
-            :src="modalMedia.url"
-            alt="Image"
-            class="w-full rounded-lg shadow-lg"
-          />
-          <video
-            v-else-if="modalMedia.type === 'video'"
-            :src="modalMedia.url"
-            controls
-            class="w-full rounded-lg shadow-lg"
-          ></video>
+            <div class="flex justify-center">
+              <img
+                v-if="modalMedia.type === 'image'"
+                :src="modalMedia.url"
+                alt="Image"
+                height="auto"
+                class="text-center rounded-lg shadow-lg max-h-screen"
+              />
+              <video
+                v-else-if="modalMedia.type === 'video'"
+                :src="modalMedia.url"
+                controls
+                class="w-full rounded-lg shadow-lg"
+              ></video>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -143,6 +161,10 @@ const openModal = (index) => {
 const closeModal = () => {
   modalMedia.value = null;
   modalIndex.value = null;
+};
+
+const closeModalOutside = () => {
+  closeModal();
 };
 
 const prevMedia = () => {
@@ -239,5 +261,10 @@ onMounted(async () => {
   top: 10px;
   right: 10px;
   cursor: pointer;
+}
+
+.img-thumbnail {
+  object-fit: cover;
+  height: 200px;
 }
 </style>
